9d46aa629c1162529d6ec584ea81fbcf06745431
LucaIlie <ilie.luca@elev.cihcahul.md>
2021-02-17 14:35:37 +0200

    hello new branch

--- lab2/Program.cs
+++ lab2/Program.cs
@@ -5,8 +5,8 @@ namespace lab2
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        
+            Console.WriteLine("Hello new branch!");
 			Console.ReadLine();
         }
     }
